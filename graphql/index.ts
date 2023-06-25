7d3e8fb641ee6baaf00d38ecea69cd759ff5a8db
ikigamisama <ikigamidevs.15@gmail.com>
2023-06-25 20:54:48 +0800

    Fixes

--- graphql/index.ts
+++ graphql/index.ts
@@ -55,34 +55,34 @@ export const createUserMutation = `
 `
 
 export const projectsQuery = `
-    query getProjects($category: String, $endcursor: String){
-        projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}){
-            pageInfo{
-                hasNextPage
-                hasPreviousPage
-                startCursor
-                endCursor
-            }
-            edges{
-                node{
-                    title
-                    githubUrl
-                    description
-                    liveSiteUrl
-                    id
-                    image
-                    category
-                    createdBy{
-                        id
-                        email
-                        name
-                        avatarUrl
-                    }
-                }
-            }
+  query getProjects($category: String, $endcursor: String) {
+    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          githubUrl
+          description
+          liveSiteUrl
+          id
+          image
+          category
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
         }
+      }
     }
-`
+  }
+`;
 
 export const getProjectByIdQuery = `
     query GetProjectById($id: ID!){
