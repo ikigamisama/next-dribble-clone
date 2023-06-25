d1c5f8dd3351f4806022cc044061d2371f99b2ac
ikigamisama <ikigamidevs.15@gmail.com>
2023-06-25 21:33:50 +0800

    Fixed for real

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -132,7 +132,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
         type="url"
         title="Website URL"
         state={form.liveSiteUrl}
-        placeholder="https://jsmastery.pro"
+        placeholder="https://ikigamidevs.com"
         setState={(value) => handleStateChange("liveSiteUrl", value)}
       />
 
@@ -140,7 +140,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
         type="url"
         title="GitHub URL"
         state={form.githubUrl}
-        placeholder="https://github.com/adrianhajdin"
+        placeholder="https://github.com/iikigami"
         setState={(value) => handleStateChange("githubUrl", value)}
       />
 
